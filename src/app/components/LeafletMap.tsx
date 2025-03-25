1c5174d59962d67e6f9de5c08bb61654cc4f538e
okkop <okkop@invalid.com>
2025-03-25 15:36:01 +0200

    Add Lealflet map. Install Leaflet and its types

--- src/app/components/LeafletMap.tsx
+++ src/app/components/LeafletMap.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { POIMarker } from "@/types/marker";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import L, { LatLngExpression } from "leaflet";
 
 type Props = {
     markers: POIMarker[],
@@ -14,12 +15,37 @@ type Props = {
  */
 export default function LeafletMap({ markers }: Props) {
 
+    const defaultCoordinates: LatLngExpression = [60.166245, 24.901596];
+
+    const mapRef = useRef<L.Map | null>(null);
+    const mapDivRef = useRef<HTMLDivElement>(null);
+
+    useEffect( () => {
+
+        if (mapDivRef.current !== null && mapRef.current == null) {
+            console.info(mapDivRef.current)
+            
+            // Init map
+            mapRef.current = L.map(mapDivRef.current, {
+                // ...
+            }).setView(defaultCoordinates, 13);
+
+            // Add default layer
+            L.tileLayer(
+                "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+                }
+            ).addTo(mapRef.current);
+
+        }
+    }, []);
+
     useEffect(() => {
         console.info("props", markers);
     }, [markers])
 
-    return (<div id="map" style={{ border: '1px solid blue', }}>
 
-    </div>);
+    return (
+        <div id="map" ref={mapDivRef} style={{ border: '1px solid blue', height: '500px' }}></div>
+    );
 
 }
