7db66fac3ffda333c8d9f0b693178e8d07a7e8d1
okkop <okkop@invalid.com>
2025-03-27 12:07:49 +0200

    Add login link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <h1>Keypro App</h1>
+      <Link href="/map">Map</Link>
     </div>
   );
 }
